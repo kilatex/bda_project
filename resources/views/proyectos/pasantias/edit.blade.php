eaf0f703d54aba098b7a3db15f0eb8a78c890855
cristhianvr24 <cristhian24vr@gmail.com>
2022-07-06 08:44:55 -0430

    arreglo edit

--- resources/views/proyectos/pasantias/edit.blade.php
+++ resources/views/proyectos/pasantias/edit.blade.php
@@ -256,7 +256,7 @@
                     <div class="row justify-content-center">
                         <div class="col-md-8">
                             <div class="card">
-                                <div class="card-header">{{ __('Registrar Tutor Acádemico') }}</div>
+                                <div class="card-header">{{ __('Actualizar Tutor Acádemico') }}</div>
                 
                                 <div class="card-body">
                                     <form method="POST" action="{{ route('tutorac.update') }}" enctype="multipart/form-data" >
@@ -375,8 +375,111 @@
           </h2>
           <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
             <div class="accordion-body">
-              <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-            </div>
+                <div class="container">
+
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                            <div class="card">
+                                <div class="card-header">{{ __('Actualizar Tutor Institucional') }}</div>
+                
+                                <div class="card-body">
+                                    <form method="POST" action="{{ route('tutorin.update') }}" enctype="multipart/form-data" >
+                                    @csrf
+                
+                                        <div class="row mb-3">
+                                            <label for="nombres" class="col-md-4 col-form-label text-md-end">{{ __('Nombres') }}</label>
+                
+                                            
+                                            <div class="col-md-6">
+                                                <input id="nombres" name="nombres" value= "{{old('nombres', $tutori->nombres)}}" type="text" class="form-control @error('nombres') is-invalid @enderror"   autofocus>
+                
+                                                @error('nombres')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror 
+                                            </div>
+                                        </div>
+                                        <div class="row mb-3">
+                                            <label for="apellidos" class="col-md-4 col-form-label text-md-end">{{ __('Apellidos') }}</label>
+                
+                                            
+                                            <div class="col-md-6">
+                                                <input id="apellidos" name="apellidos" value= "{{old('apellidos', $tutori->apellidos)}}" type="text" class="form-control @error('apellidos') is-invalid @enderror"   autofocus>
+                
+                                                @error('apellidos')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror 
+                                            </div>
+                                        </div>
+                                        <div class="row mb-3">
+                                            <label for="cedula" class="col-md-4 col-form-label text-md-end">{{ __('Cedula') }}</label>
+                
+                                            
+                                            <div class="col-md-6">
+                                                <input id="cedula" name="cedula" value= "{{old('cedula', $tutori->cedula)}}" type="text" class="form-control @error('cedula') is-invalid @enderror"   autofocus>
+                
+                                                @error('cedula')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror 
+                                            </div>
+                                        </div>
+                                        <div class="row mb-3">
+                                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Correo Electronico') }}</label>
+                                            <div class="col-md-6">
+                                                <input id="email" name="email" value= "{{old('email', $tutori->email)}}" type="email" class="form-control @error('email') is-invalid @enderror"   autofocus>
+                
+                                                @error('email')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror 
+                                            </div>
+                                        </div>
+                                        <div class="row mb-3">
+                                            <label for="telefono" class="col-md-4 col-form-label text-md-end">{{ __('Telefono') }}</label>
+                                            <div class="col-md-6">
+                                                <input id="telefono" name="telefono" value= "{{old('telefono', $tutori->telefono)}}" type="text" class="form-control @error('telefono') is-invalid @enderror"   autofocus>
+                
+                                                @error('telefono')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror 
+                                            </div>
+                                        </div>
+                                        <div class="row mb-3">
+                                            <label for="nombre_especialidad" class="col-md-4 col-form-label text-md-end">{{ __('Especialidad') }}</label>
+                
+                                            
+                                            <div class="col-md-6">
+                                                <input id="nombre_especialidad" name="nombre_especialidad" value= "{{old('nombre_especialidad', $tutori->nombre_especialidad)}}" type="text" class="form-control @error('nombre_especialidad') is-invalid @enderror"   autofocus>
+                
+                                                @error('nombre_especialidad')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror 
+                                            </div>
+                                        </div>
+                                        <div class="row mb-0">
+                                            <div class="col-md-6 offset-md-4">
+                                                    <button type="submit" class="btn btn-primary">
+                                                        {{ __('Actualizar Tutor Institucional') }}
+                                                    </button>
+                                                </div>
+                                        </div>  
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>            </div>
           </div>
         </div>
       </div>
